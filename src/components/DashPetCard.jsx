0beea903c0e280eb0a625c03cbd5d62fe66d1e86
SOCRAMBLLE <mfragaprt@me.com>
2024-04-17 14:55:13 +0200

    Dashboard pet card updated - Added food info

--- src/components/DashPetCard.jsx
+++ src/components/DashPetCard.jsx
@@ -3,10 +3,12 @@ import "./DashPetCard.css";
 import PropTypes from "prop-types";
 import pawIcon from "../assets/brand/paw.png";
 import { PiDotOutlineFill } from "react-icons/pi";
+import buddyBowlEmpty from "../assets/misc/buddy-bowl.png";
+import buddyBowlFull from "../assets/misc/buddy-bowl-full.png";
+import buddyBowlHalf from "../assets/misc/buddy-bowl-half.png";
 
 const DashPetCard = ({ pet, id }) => {
   const { pathname } = useLocation();
-
   let petImage = pawIcon;
 
   const petAge = (birthday) => {
@@ -19,6 +21,31 @@ const DashPetCard = ({ pet, id }) => {
     }
     return age;
   };
+
+  const getTodaysFoodIntake = () => {
+    const today = new Date().toISOString().slice(0, 10);
+    const todaysEntries = pet.foodIntake.filter(
+      (entry) => entry.date.slice(0, 10) === today
+    );
+    return todaysEntries.reduce((total, entry) => total + entry.grams, 0);
+  };
+
+  const todaysIntake = getTodaysFoodIntake();
+  const dailyGoal = pet.foodGrams;
+
+  let bowlImage;
+  let statusMessage;
+
+  if (todaysIntake === 0) {
+    bowlImage = buddyBowlEmpty;
+    statusMessage = "No food recorded yet for today.";
+  } else if (todaysIntake < dailyGoal) {
+    bowlImage = buddyBowlHalf;
+    statusMessage = `${pet.name} has eaten ${todaysIntake}g of ${dailyGoal}g today.`;
+  } else {
+    bowlImage = buddyBowlFull;
+    statusMessage = `${pet.name} already have eaten the daily dose today ✅`;
+  }
 
   return (
     <div className="dash-pet">
@@ -47,7 +74,12 @@ const DashPetCard = ({ pet, id }) => {
           </div>
         </div>
       </Link>
-      <div className="dash-pet--infos">infos</div>
+      <div className="dash-pet--infos">
+        <Link to={`food/${pet.id}/history`}>
+          <img src={bowlImage} />
+          <p>{statusMessage}</p>
+        </Link>
+      </div>
     </div>
   );
 };
